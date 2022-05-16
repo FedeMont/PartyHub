b05d723ce4de4d6edec5c31fb0e1f7752deb0098
Federico Montagna <fedemonti00@gmail.com>
2022-05-16 15:54:39 +0200

    initial Api commit

--- node/app.js
+++ node/app.js
@@ -19,6 +19,8 @@ const options = {
 
 const swaggerSpec = swaggerJsdoc(options);
 
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
