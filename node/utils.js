6442ca990e20172ff224e757d1d8919f038dc736
Jprogrammer13 <buss.riky@gmail.com>
2022-05-19 12:39:29 +0200

    Implemented and tested crea_service api

--- node/utils.js
+++ node/utils.js
@@ -29,12 +29,18 @@ const geopositionSchema = new mongoose.Schema({
     label: { type: String, required: true }
 });
 
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    // icon: Image()
+});
+
 let documents = {
     userSchema: new mongoose.Schema({
         _id: mongoose.Schema.Types.ObjectId,
         name: { type: String, required: true },
         surname: { type: String, required: true },
-        username: { type: String, required: true, unique: true},
+        username: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         account_type: { type: String, required: true, default: "up" },
@@ -88,7 +94,7 @@ let documents = {
             required: true
         },
         start_datetime: { type: Date, required: true },
-        end_datetime: { type: Date, required: true,  },
+        end_datetime: { type: Date, required: true, },
         // poster: Image(),
         age_range_min: { type: Number, required: true },
         age_range_max: { type: Number, required: true },
@@ -100,7 +106,18 @@ let documents = {
             type: mongoose.Schema.Types.ObjectId,
             ref: "GalleryPhoto"
         }],
-        owner: { type: mongoose.Schema.Types.ObjectId, required: true }
+        owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" }
+    }),
+    productSchema: productSchema,
+    serviceSchema: new mongoose.Schema({
+        _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+        owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+        name: { type: String, required: true },
+        number_of_products: { type: Number, required: true },
+        products_list: [{
+            type: productSchema,
+            required: true
+        }]
     })
 };
 
