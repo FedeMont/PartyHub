a977807ce40ce341254c97930d6e91dd424605b4
Alice <maps102546@gmail.com>
2022-05-27 10:27:00 +0200

    implemented lista dipendenti api

--- node/api/dipendente/dipendente.js
+++ node/api/dipendente/dipendente.js
@@ -281,4 +281,146 @@ routes.post('/activate_turno', authenticateToken, (req, res) => {
     }
 });
 
+
+/**
+ * @openapi
+ * paths:
+ *  /api/dipendente/get_dipendenti:
+ *      get:
+ *          summary: Ritorna i dipendenti
+ *          description: Restituisce i dipendenti dell'utente loggato
+ *          security:
+ *              - bearerAuth: []
+ *          responses:
+ *              200:
+ *                  description: Dipendenti.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: integer
+ *                                      description: http status.
+ *                                      example: 200
+ *                                  message:
+ *                                      type: array
+ *                                      items:
+ *                                          type: object
+ *                                          properties:
+ *                                              _id:
+ *                                                  type: string
+ *                                                  description: Id del dipendente.
+ *                                                  example: 6288ec25fe5bb453c76a62fa
+ *                                              name:
+ *                                                  type: string
+ *                                                  description: Nome del dipendente.
+ *                                                  example: Nome dipendente
+ *                                              cognome:
+ *                                                  type: string
+ *                                                  description: Cognome del dipendente.
+ *                                                  example: Cognome dipendente
+ *                                              number_of_services:
+ *                                                  type: integer
+ *                                                  description: Numero di servizi appartenenti al dipendente.
+ *                                                  example: 3
+ *                                              services_list:
+ *                                                  type: array
+ *                                                  items:
+ *                                                      type: string
+ *                                                      description: Nome del servizio.
+ *                                                      example: Servizio
+ *                                              number_of_events:
+ *                                                  type: integer
+ *                                                  description: Numero di eventi appartenenti al dipendente.
+ *                                                  example: 3
+ *                                              events_list:
+ *                                                  type: array
+ *                                                  items:
+ *                                                      type: string
+ *                                                      description: Nome dell' evento.
+ *                                                      example: Evento
+ *              401:
+ *                  description: Token email errata.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: integer
+ *                                      description: http status.
+ *                                      example: 401
+ *                                  message:
+ *                                      type: string
+ *                                      description: messaggio.
+ *                                      example: Token email errata.
+ *              500:
+ *                  description: Errore nella ricerca di utente.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  status:
+ *                                      type: integer
+ *                                      description: http status.
+ *                                      example: 500
+ *                                  message:
+ *                                      type: string
+ *                                      description: messaggio.
+ *                                      example: Errore nella ricerca di utente.
+ */
+routes.get('/get_dipendenti', authenticateToken, (req, res) => {
+    User.find({ $and: [{ email: req.user.mail }, { account_type: "o" }] }, "", (err, users) => {
+        if (errHandler(res, err, "utente")) {
+
+            if (users.length === 0) return standardRes(res, 401, "Non ti è possibile cercare dipendenti.");
+
+            let user = users[0];
+            console.log(user);
+
+            User.find({ $and: [{ _id: user.dipendenti_list }, { account_type: "d" }] }, "name surname email events_list services_list").populate("events_list").populate("services_list").exec().then(dipendenti => {
+                console.log(dipendenti);
+
+                let to_return = [];
+
+                dipendenti.forEach(dipendente => {
+                    dipendente_info = {};
+                    dipendente_info["_id"] = dipendente._id;
+                    dipendente_info["name"] = dipendente.name;
+                    dipendente_info["surname"] = dipendente.surname;
+                    dipendente_info["email"] = dipendente.email;
+
+                    dipendente_info["events_list"] = [];
+                    dipendente.events_list.forEach(event => {
+                        let events_list = {};
+                        events_list["name"] = event.name;
+                        events_list["start_datetime"] = event.start_datetime;
+                        events_list["address"] = event.address.label;
+                        dipendente_info["events_list"].push(events_list);
+                    });
+
+                    console.log(dipendente_info.events_list);
+                    dipendente_info["services_list"] = [];
+                    dipendente.services_list.forEach(service => {
+                        let services_list = {};
+                        services_list["name"] = service.name;
+                        dipendente_info["services_list"].push(services_list);
+                    });
+
+                    to_return.push(dipendente_info);
+                });
+
+                console.log(to_return);
+
+                return standardRes(res, 200, to_return);
+            })
+                .catch(err => {
+                    errHandler(res, err, "dipendenti");
+                });
+        }
+    });
+});
+
 module.exports = routes;
