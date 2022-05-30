059444f082a9637349a155c3e0f1ce188aba87c5
Federico Montagna <fedemonti00@gmail.com>
2022-05-30 14:26:44 +0200

    Added storico eventi organizzatore ui

--- node/app.js
+++ node/app.js
@@ -51,6 +51,7 @@ app.use('/organizzatore/', express.static(path.join(__dirname, '/ui/organizzator
 app.use('/organizzatore/crea_servizio', express.static(path.join(__dirname, '/ui/organizzatore/services/crea_service')));
 app.use('/organizzatore/crea_evento', express.static(path.join(__dirname, '/ui/organizzatore/events/crea_evento')));
 app.use('/organizzatore/lista_dipendenti', express.static(path.join(__dirname, '/ui/organizzatore/dipendenti/lista_dipendenti')));
+app.use('/organizzatore/storico_eventi', express.static(path.join(__dirname, '/ui/organizzatore/events/storico_eventi')));
 app.use('/organizzatore/crea_dipendente', express.static(path.join(__dirname, '/ui/organizzatore/dipendenti/crea_dipendente')));
 app.use('/organizzatore/event/gallery', express.static(path.join(__dirname, '/ui/organizzatore/events/gallery')));
 // end - organizzatore
